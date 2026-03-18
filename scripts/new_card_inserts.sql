de3dbc451e59ba05a641a8430560fe2b688ae8c4
Talmon Friedlander <talmonf@gmail.com>
2026-03-18 21:24:37 +0200

    File for Insert sqls

---
+++ scripts/new_card_inserts.sql
@@ -0,0 +1,23 @@
+-- Inserts for newly added directory_entries cards.
+-- Each time new cards are added, append the corresponding INSERT(s) to this file.
+
+INSERT INTO directory_entries (
+  entry_id,
+  display_name,
+  description,
+  primary_category,
+  category_keys
+)
+VALUES
+  ('vivid_the_flexible_app_for_life_changing_practices_102', 'VIVID - The Flexible App for Life-Changing Practices', 'אפליקציה אינטראקטיבית לבניית תוכניות תרגול יומי ומעקב לאורך זמן.', 'anxiety_apps', ARRAY['anxiety_apps']::text[]);
+
+INSERT INTO directory_entries (
+  entry_id,
+  display_name,
+  description,
+  primary_category,
+  category_keys
+)
+VALUES
+  ('המדריך_ל_ptsd_103', 'המדריך ל-PTSD', 'אפליקציה/מדריך דיגיטלי להתמודדות עם PTSD, מטעם אגף השיקום (משרד הביטחון).', 'anxiety_apps', ARRAY['anxiety_apps']::text[]);
+
