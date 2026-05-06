1da980e2779560d74466ce787fafca888e652aa5
Talmon Friedlander <talmonf@gmail.com>
2026-05-06 21:20:45 +0300

    6 may fixes - 2

--- scripts/004_directory_entries_emergency_local_patch.sql
+++ scripts/004_directory_entries_emergency_local_patch.sql
@@ -15,4 +15,8 @@ UPDATE directory_entries
 SET category_keys = ARRAY['therapists', 'local']::text[]
 WHERE entry_id = 'רזולוציה_205';
 
+UPDATE directory_entries
+SET description = 'קווי המענה הארציים של ארגון הגג; סיוע לחברה הדתית (*2511, 02-5328000); צ''אט אנונימי וסיוע ב-WhatsApp (קולמילה).'
+WHERE entry_id = 'איגוד_מרכזי_הסיוע_לנפגעות_ולנפגעי_תקיפה_מינית_200';
+
 COMMIT;
